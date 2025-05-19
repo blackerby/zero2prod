6492608da184a7e29bede71bf7dd792097f49a8c
William Blackerby <wmblackerby@gmail.com>
2025-05-19 09:54:20 -0400

    through 3.7

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,6 @@ use zero2prod::run;
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
-println!("hello");
     let listener = TcpListener::bind("127.0.0.1:8000")?;
     run(listener)?.await
 }
