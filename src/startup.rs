2a6024586fe0d740ae0be807a40c5f2a85ff6a57
William Blackerby <wmblackerby@gmail.com>
2025-06-24 17:15:10 -0400

    through 4.3

--- src/startup.rs
+++ src/startup.rs
@@ -1,7 +1,9 @@
 use actix_web::dev::Server;
+use actix_web::middleware::Logger;
 use actix_web::web;
 use actix_web::App;
 use actix_web::HttpServer;
+use env_logger::Env;
 use sqlx::PgPool;
 
 use std::net::TcpListener;
@@ -9,9 +11,11 @@ use std::net::TcpListener;
 use crate::routes::{health_check, subscribe};
 
 pub fn run(listener: TcpListener, db_pool: PgPool) -> Result<Server, std::io::Error> {
+    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
     let db_pool = web::Data::new(db_pool);
     let server = HttpServer::new(move || {
         App::new()
+            .wrap(Logger::default())
             .route("/health_check", web::get().to(health_check))
             .route("/subscriptions", web::post().to(subscribe))
             .app_data(db_pool.clone())
